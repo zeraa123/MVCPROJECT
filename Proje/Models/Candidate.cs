ea0d7ce522980cc947f7a07e95eb8b1b6a86b140
oguzhan beyaz <beyaz@gmail.com>
2023-05-30 16:43:25 +0300

    Second commit

---
+++ Proje/Models/Candidate.cs
@@ -0,0 +1,11 @@
+namespace Proje.Models
+{
+    public class Candidate
+    {
+        public String? Name{get; set;}=String.Empty;
+        public String? Surname{get; set;}=String.Empty;
+        public int? Age{get; set;}
+        public DateTime? Apply{get; set;}
+        
+    }
+}
